988d93dcd83a6e533593f9627f6e413cac0d675e
Crilum <91354257+Crilum@users.noreply.github.com>
2023-02-27 12:44:09 -0700

    Add random punctuation, emojis, and add logging

--- index.js
+++ index.js
@@ -6,18 +6,18 @@ var finalTemplates = [
 ];
 
 var templates = [
-  function () { return "I've always thought " + getFinalTemplate() + "." },
-  function () { return "9 out of 10 doctors say " + getFinalTemplate() + "." },
-  function () { return "1 out of 10 doctors say " + getFinalTemplate() + ", but that's good enough, right?" },
-  function () { return "all 10 of those doctors say " + getFinalTemplate() + "!" },
-  function () { return "The Armpit Hair Fairy told me " + getFinalTemplate() + "..." },
-  function () { return "people say " + getFinalTemplate() + "." },
-  function () { return "I think " + getFinalTemplate() + "." },
-  function () { return "you know, " + getFinalTemplate() + "." },
-  function () { return "I hear " + getFinalTemplate() + "." },
-  function () { return "a little birdie told me " + getFinalTemplate() + "." },
-  function () { return "I've always wanted to tell you, " + getFinalTemplate() + "." },
-  function () { return "did you know that " + getFinalTemplate() + "?" },
+  function () { return "I've always thought " + getFinalTemplate() },
+  function () { return "9 out of 10 doctors say " + getFinalTemplate() },
+  function () { return "1 out of 10 doctors say " + getFinalTemplate() + ", but that's good enough, right" },
+  function () { return "all 10 of those doctors say " + getFinalTemplate() },
+  function () { return "The Armpit Hair Fairy told me " + getFinalTemplate() },
+  function () { return "people say " + getFinalTemplate() },
+  function () { return "I think " + getFinalTemplate() },
+  function () { return "you know, " + getFinalTemplate() },
+  function () { return "I hear " + getFinalTemplate() },
+  function () { return "a little birdie told me " + getFinalTemplate() },
+  function () { return "I've always wanted to tell you, " + getFinalTemplate() },
+  function () { return "did you know that " + getFinalTemplate() },
 ].concat(finalTemplates);
 
 var abstractProperties = [
@@ -25,10 +25,72 @@ var abstractProperties = [
   function () { return [getDescriptor(), getTangibleMultipleProperty()].join(" "); },
 ];
 
+const colors = {
+  yellow: "\x1b[33m",
+  blue: "\x1b[36m",
+  red: "\x1b[31m",
+  grey: "\x1b[38;5;254m",
+  green: "\x1b[38;5;47m",
+  NC: "\x1b[0m"
+}
 
+function colorize(color, message) {
+  if (color == "blue") {
+    return `${colors.blue}${message}${colors.NC}`
+  } else if (color == "yellow") {
+    return `${colors.yellow}${message}${colors.NC}`
+  } else if (color == "red") {
+    return `${colors.red}${message}${colors.NC}`
+  } else if (color == "grey") {
+    return `${colors.grey}${message}${colors.NC}`
+  } else if (color == "green") {
+    return `${colors.green}${message}${colors.NC}`
+  } else {
+    console.log(`${colors.yellow}Warning: ${colors.red}Garbage color argument!${colors.NC}`)
+    return `${message}`
+  }
+}
 
-var makeCompliment = module.exports = function makeCompliment() {
-  return getTemplate();
+function current_date() {
+  let currentDate = new Date();
+  let day = currentDate.getDate();
+  let month = currentDate.getMonth() + 1;
+  let year = currentDate.getFullYear();
+  let date = `${month}-${day}-${year}`
+  let time = currentDate.getHours() + ":" + currentDate.getMinutes() + ":" + currentDate.getSeconds() + ":" + currentDate.getMilliseconds();
+  return `${time} ${date} ~ `
+}
+
+function log(message) {
+  console.log(colorize("grey", current_date()), colorize("blue", message));
+}
+
+var makeCompliment = module.exports = function makeCompliment(isNice) {
+  if (isNice == "nice") {
+    let compliment = getTemplate() + getRandomPunctuation();
+    let a = check_compliment_is_nice(compliment)
+    while (a == 1) {
+        log("Regenerating compliment...")
+        compliment = getTemplate() + getRandomPunctuation();
+        a = check_compliment_is_nice(compliment)
+    }
+    return compliment;
+  } else if (isNice == "roast") {
+    let compliment = getTemplate() + getRandomPunctuation();
+    let a = check_compliment_is_nice(compliment)
+    while (a == 0) {
+        log("Regenerating compliment...")
+        compliment = getTemplate() + getRandomPunctuation();
+        a = check_compliment_is_nice(compliment)
+    }
+    return compliment;
+  } else if (isNice == "random" || isNice == "" || isNice == undefined || isNice == null) {
+    return getTemplate() + getRandomPunctuation();
+  }
+};
+
+var getRandomPunctuation = module.exports.getRandomPunctuation = function getRandomPunctuation() {
+  return chars[Math.floor((Math.random() * chars.length))]
 };
 
 var makeSingularForm = module.exports.makeSingularForm = function makeSingularForm(text) {
@@ -71,6 +133,102 @@ var getDescriptor = module.exports.getDescriptor = function getDescriptor() {
   return [getAdverb(), getAdjective()].join(" ");
 }
 
+var check_compliment_is_nice = module.exports.check_compliment_is_nice = function check_compliment_is_nice(compliment) {
+  for (let i = 0; i < badWords.length; i++) {
+    if (compliment.includes(badWords[i])) {
+      log(`Includes bad phrase: ${badWords[i]}`)
+      return 1
+    }
+  };
+  for (let i = 0; i < badChars.length; i++) {
+    if (compliment.includes(badChars[i])) {
+      log(`Includes bad punctuation: ${badChars[i]}`)
+      return 1
+    }
+  };
+  return 0
+}
+
+const badWords = [
+  "almost",
+  "I am",
+  "nearly",
+  "kind of",
+  "practically",
+  "virtually",
+  "somewhat",
+  "terribly",
+  "faintly",
+  "fairly",
+  "deftly",
+  "1 out of 10",
+  "nose",
+  "Armpit Hair",
+  "typos",
+  "acceptable",
+  "breakable",
+  "combustible",
+  "durable",
+  "flammable",
+  "flexible",
+  "forgettable",
+  "illegible",
+  "impeachable",
+  "implausible",
+  "improbable",
+  "laughable",
+  "managable",
+  "questionable",
+  "tolerable",
+  "unacceptable",
+  "unamiable",
+  "uncategorizable",
+  "eatable",
+  "uneatable",
+  "unlovable",
+  "unvaluable",
+  "weird",
+  "crappy",
+]
+
+const chars = [
+  ".",
+  "?",
+  "!",
+  "..",
+  "...",
+  "??",
+  "!?!?!",
+  "!!!",
+  " ?!",
+  " 😉",
+  " 🙃",
+  " 👀",
+  " 😎",
+  " 😳",
+  " 😼",
+  " 🤗",
+  " 🤭",
+  " 💥",
+  " 👌",
+  " 🔥",
+  " 😈",
+]
+const badChars = [
+  "..",
+  "...",
+  "?!",
+  "!?!?!",
+  "?",
+  "??",
+  "!!!",
+  " 👀",
+  " 😳",
+  " 😼",
+  " 💥",
+  " 😈",
+]
+
 const tangibleSingularProperties = [
   "beauty",
   "being",
@@ -97,10 +255,10 @@ const tangibleSingularProperties = [
   "voice",
   "way of thinking",
   "wonder",
-  "words"
 ]
 const tangibleMultipleProperties = [
   "accomplishments",
+  "compliments",
   "eyebrows",
   "eyes",
   "ideas",
@@ -110,7 +268,8 @@ const tangibleMultipleProperties = [
   "thoughts",
   "thumbs",
   "typos",
-  "ears"
+  "ears",
+  "words"
 ]
 const adjectives = [
   "acceptable",
@@ -127,7 +286,7 @@ const adjectives = [
   "dependable",
   "desirable",
   "durable",
-  "eatable",
+  "edible",
   "enviable",
   "fashionable",
   "flammable",
---
+++ test.js
@@ -0,0 +1,5 @@
+const complimenter = require("./index.js")
+
+console.log("Nice compliment: " + complimenter("nice"))
+console.log("Random compliment: " + complimenter("random"))
+console.log("Roast: " + complimenter("roast"))
